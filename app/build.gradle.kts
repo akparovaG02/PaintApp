c4d152e81cff7ae820a3acfd8477ecd78b8b925b
Akparova Gulnara <akparovag.03@gmail.com>
2024-07-05 23:47:45 +0500

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.paintapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.paintapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation ("com.kyanogen.signatureview:signature-view:1.2")
+    implementation ("com.karumi:dexter:6.2.3")
+    implementation ("com.github.yukuku:ambilwarna:2.0.1")
+
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
+
+
+
+}
